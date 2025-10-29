bb38a36e70425a0b080f05dcfd74bfa4786d7551
kdaehun00 <kdaehun00@naver.com>
2025-10-29 17:39:12 +0900

    review: [service] return 형식 일관성 보장

--- src/main/java/com/wjc/codetest/product/service/ProductService.java
+++ src/main/java/com/wjc/codetest/product/service/ProductService.java
@@ -34,6 +34,13 @@ public class ProductService {
         return productOptional.get();
     }
 
+    /*
+    문제: 메서드 간 반환 일관성 부족
+    원인: create()에서는 save() 결과를 직접 반환하지만,
+         update()에서는 별도 변수(updatedProduct)에 담아 반환하고 있음
+    개선안: 두 메서드 모두 동일한 반환 패턴으로 통일 (return productRepository.save(...))
+    효과: 코드 일관성 및 가독성 향상
+    */
     public Product update(UpdateProductRequest dto) {
         Product product = getProductById(dto.getId());
         product.setCategory(dto.getCategory());
