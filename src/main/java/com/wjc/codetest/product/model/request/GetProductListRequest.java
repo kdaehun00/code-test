629c8c139e465685e3193e06f839dab46b3a64b8
kdaehun00 <kdaehun00@naver.com>
2025-10-29 15:41:50 +0900

    review: [request] git newLine 문제

--- src/main/java/com/wjc/codetest/product/model/request/GetProductListRequest.java
+++ src/main/java/com/wjc/codetest/product/model/request/GetProductListRequest.java
@@ -10,3 +10,9 @@ public class GetProductListRequest {
     private int page;
     private int size;
 }
+
+/*
+문제: GitHub 푸시 시 NewLine 관련 에러 발생 우려
+원인: 파일 끝에 개행 문자 미삽입
+개선안: IDE 설정을 통해 저장 시 자동으로 개행 문자 삽입
+*/
