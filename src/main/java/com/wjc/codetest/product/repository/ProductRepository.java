0c0efc131243f43a27042d6b496cf61cb01e83e0
kdaehun00 <kdaehun00@naver.com>
2025-10-29 22:55:20 +0900

    review: [repository] DISTINCT 활용 시 full table scan 가능성

--- src/main/java/com/wjc/codetest/product/repository/ProductRepository.java
+++ src/main/java/com/wjc/codetest/product/repository/ProductRepository.java
@@ -15,6 +15,23 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByCategory(String name, Pageable pageable);
 
+    /*
+    문제: DISTINCT로 인한 풀 테이블 스캔 발생
+    원인:
+      - DISTINCT는 모든 행을 읽어 중복 제거 필요
+      - 인덱스 활용 어려움
+    개선안:
+      - GROUP BY를 사용하여 인덱스 활용
+      - category 테이블을 따로 분리
+    효과:
+      - 인덱스 활용으로 성능 개선
+      - 풀 테이블 스캔 방지
+    좋아보이는 방식:
+      - category 테이블 분리
+    이유:
+      - 현재는 category가 변하면 관련된 모든 Product의 category 컬럼을 변경해야함.
+      - 테이블을 분리하면 특정 값만 변경하면 됨.
+    */
     @Query("SELECT DISTINCT p.category FROM Product p")
     List<String> findDistinctCategories();
 }
