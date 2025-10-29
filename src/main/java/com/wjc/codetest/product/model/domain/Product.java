5261646779b0fc5d6a48878f172e35bcfae8b754
kdaehun00 <kdaehun00@naver.com>
2025-10-29 11:43:10 +0900

    review: [domain] Setter 사용

--- src/main/java/com/wjc/codetest/product/model/domain/Product.java
+++ src/main/java/com/wjc/codetest/product/model/domain/Product.java
@@ -6,6 +6,11 @@ import lombok.Setter;
 
 @Entity
 @Getter
+/*
+문제: 객체 불변성이 훼손됨, 사이드 이펙트 추적의 어려움.
+원인: Setter를 Class 단에서 사용하면 모든 필드에 public setter 생성 -> 내부 데이터가 의도치 않게 변할 우려가 있음.
+개선안: 1. 일부 데이터만 setter를 사용한다. 2. 유연성이 필요한 경우 메서드로 분리한다.
+*/
 @Setter
 public class Product {
 
