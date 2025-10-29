2b29cb181063c0a117ef8bc7bc404d6bfedadca3
kdaehun00 <kdaehun00@naver.com>
2025-10-29 15:34:15 +0900

    review: [request] Record 사용 및 선택 필드 명시, setter 제거

--- src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
+++ src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
@@ -5,6 +5,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
+/*
+문제: 필드의 필수/선택 여부가 불명확하고 보일러플레이트 코드가 많음.
+
+원인:
+  1. 생성자 오버로딩으로 name의 필수 여부를 암시적으로만 표현
+  2. @Getter/@Setter와 생성자를 직접 작성하여 코드량 증가
+  3. setter로 인해 불변성 보장 안 됨
+
+개선안:
+  1. Java Record 사용
+  2. name이 필수값이 아니라면 Optional<String>로 명시
+    (category만 있는 것을 보아 name이 필수값이 아닌 것처럼 보임)
+
+효과:
+  - 코드 간결화
+  - 필드 필수/선택 여부가 명확
+*/
 public class CreateProductRequest {
     private String category;
     private String name;
